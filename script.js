bf89ff1a4e4844eac38463f51c953a9f72aef49f
Asako Kanno <asakok@AsakonoMacBook-Air.local>
2025-09-25 17:56:22 +0200

    add JS

--- script.js
+++ script.js
@@ -8,14 +8,57 @@ btns.forEach((btn) => {
     })
 })
 
+// btns.forEach((btn) => {
+//     btn.addEventListener("click", () => {
+//         const targetID = btn.textContent.trim().toLowerCase();
+//         const template = document.querySelector(`#templates #${targetID}`);
+
+//         if (template) {
+//             const clone = template.cloneNode(true);
+//             container.appendChild(clone);
+//         }
+//     });
+// });
+
 btns.forEach((btn) => {
     btn.addEventListener("click", () => {
-        const targetID = btn.textContent.trim().toLowerCase();
-        const template = document.querySelector(`#templates #${targetID}`);
+        const textContainer = document.querySelector("#text-container");
+            if (btn.textContent.trim() === "All") {
+                const p = document.createElement("p");
+                p.textContent = "You eat everything, maybe liver then?";
+                textContainer.appendChild(p);
+            }
 
-        if (template) {
-            const clone = template.cloneNode(true);
-            container.appendChild(clone);
-        }
+            if (btn.textContent.trim() === "Italy") {
+                const p = document.createElement("p");
+                p.textContent = "Are you a pasta or a pizza person?";
+                textContainer.appendChild(p)
+            }
+
+            if (btn.textContent.trim() === "USA") {
+                const p = document.createElement("p");
+                p.textContent = "Cranberry sauce?";
+                textContainer.appendChild(p);
+            }
+
+            if (btn.textContent.trim() === "China") {
+                const p = document.createElement("p");
+                p.textContent = "You chose Chinese";
+                textContainer.appendChild(p);
+            }
+
+            if (btn.textContent.trim() === "Descending") {
+                const p = document.createElement("p");
+                p.textContent = "You ar in a hurry mate?";
+                textContainer.appendChild(p);
+            }
+
+            if (btn.textContent.trim() === "Ascending") {
+                const p =document.createElement("p");
+                p.textContent = "You want to impress on someone?";
+                textContainer.appendChild(p);
+            }
+
+
     });
 });
