dc5ac839a19c4924e0a8e3394de7975b8b933772
Asako Kanno <asakok@AsakonoMacBook-Air.local>
2025-10-08 19:58:07 +0200

    add backup recipes

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const URL = "https://api.spoonacular.com/recipes/random?number=10&apiKey=e1711b2ca9f84dec882725da3bd3acfd&cuisine=italian,american,chinese,european,mexican";
-
-// // Global backup data variable
-// import { backupRecipes } from "./backup.js"
-
-// // Global backup data variable
-// console.log("backup data loaded", backupRecipes);
+// const URL = "https://api.spoonacular.com/recipes/random?number=10&apiKey=e1711b2ca9f84dec882725da3bd3acfd&cuisine=italian,american,chinese,european,mexican";
 
 const allBtn = document.getElementById("all")
 const filterBtn = document.querySelectorAll(".filter-btn")
@@ -218,24 +212,46 @@ randomBtn.addEventListener("click", () => {
 // fetch recipes
 //===============================
 
+// let recipes = [];
+
+// const fetchData = async () => {
+//     try {
+//         const response = await fetch(URL);
+//         const data = await response.json();
+//         console.log("API response:", data);
+
+//         if (data.recipes && Array.isArray(data.recipes)) {
+//             recipes = data.recipes;
+//             displayRecipes(data.recipes);
+//         } else {
+//             container.innerHTML = "<p>No recipes found. Check your API key or quota.</p>";
+//         }
+//     } catch (error) {
+//         console.error("Error fetching data:", error);
+//         container.innerHTML = "<p>Failed to load recipes. Please try again later.</p>";
+//     }
+// };
+
+// ------------import backup data-----------------------------------------
+
+// Global backup data variable
+import { backupRecipes } from "./backup.js"
+
+// Global backup data variable
+console.log("backup data loaded", backupRecipes);
+
+let recipes = []; 
+
 const fetchData = async () => {
     try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        console.log("API response:", data);
-
-        if (data.recipes && Array.isArray(data.recipes)) {
-            displayRecipes(data.recipes);
-        } else {
-            container.innerHTML = "<p>No recipes found. Check your API key or quota.</p>";
-        }
+        recipes = backupRecipes.recipes;
+        displayRecipes(recipes);
     } catch (error) {
         console.error("Error fetching data:", error);
-        container.innerHTML = "<p>Failed to load recipes. Please try again later.</p>";
     }
 };
 
-
+fetchData();
 
 //===============================
 // display recipes
@@ -279,105 +295,83 @@ const displayRecipes = (recipeArray) => {
     attachLikeEvents();
 }
 
-// displayRecipes(results);
-
 fetchData()
 
 //===============================
-// filter recipes
+// filter + sort recipes
 //===============================
 
-// save the selected filter here
-let activeFilters = []
+// save the selected filters here
+let activeFilters = [];
+let currentSort = null; // asc or desc
 
-// show all recipes when "all" button is clicked
+// reset filters when "All" button is clicked
 allBtn.addEventListener("click", () => {
-    activeFilters = []; //reset filters
-    displayRecipes(recipes);
+    activeFilters = []; // reset filters
+    applyKitchenFilters();
 });
 
-// 
+// each cuisine button
 filterBtn.forEach(button => {
-    if (button.id === "all")return; // ignore the rest of the functions is "all" is clicked
+    if (button.id === "all")
+        return; // skip the "all" button
 
     button.addEventListener("click", () => {
-        const cuisine = button.id;
+        const cuisine = button.id.toLowerCase();
 
         if (activeFilters.includes(cuisine)) {
-            // remove recipes if they are already displayed 
-            activeFilters = activeFilters.filter(country => country !== cuisine);
+            // remove if already selected
+            activeFilters = activeFilters.filter(c => c !== cuisine);
         } else {
-            // add the filtered recipes
+            // add filter
             activeFilters.push(cuisine);
         }
 
-        applyFilters();
+        applyKitchenFilters();
     });
 });
 
-const applyFilters = () => {
-    // if there are no active filters, then show all recipes
-    if (activeFilters.length === 0) {
-        displayRecipes(recipes);
-        return;
-    }
-
-    const filtered = recipes.filter(recipe =>
-        activeFilters.includes(recipe.cuisine.toLowerCase())
-    );
-
-    // if there are active recipes, show the recipes. otherwise show the text
-    if (filtered.length > 0) {
-        displayRecipes(filtered);
-    } else {
-        container.innerHTML = `<p class="no-results">No results</p>`;
-    }
-}
-
-
-//===============================
-// sort recipes (sort by time)
-//===============================
+// sort buttons
 
 descBtn.addEventListener("click", () => {
     currentSort = "desc";
     applyKitchenFilters();
-})
+});
 
 ascBtn.addEventListener("click", () => {
     currentSort = "asc";
     applyKitchenFilters();
 })
 
-// filter + sort function
-const applyKitchenFilters = () => {
-    let filtered = []; // make an empty box
+// main function : filter + sort
 
-    if (activeFilters.length === 0) {
-        // if no active filter, make a copy of recipes list
-        filtered = [...recipes];
-    } else {
-        // if filters are active, pick up the recipes
-        filtered = recipes.filter(recipe =>
-            activeFilters.includes(recipe.cuisine.toLowerCase())
-        );
+const applyKitchenFilters = () => {
+    let filtered = [...recipes]; // make a copy of all recipes
+
+    //apply filters
+    if (activeFilters.length > 0) {
+        filtered = filtered.filter(recipe => {
+            if (Array.isArray(recipe.cuisines) && recipe.cuisines.length > 0) {
+                return recipe.cuisines.some(c => activeFilters.includes(c.toLowerCase()));
+            }
+            return false;
+        });
     }
 
-    // sort filtered recipes
+    // apply sorting
     if (currentSort === "desc") {
         filtered.sort((a,b) => b.readyInMinutes - a.readyInMinutes);
-    } else {
+    } else if (currentSort === "asc") {
         filtered.sort((a,b) => a.readyInMinutes - b.readyInMinutes);
     }
 
-    // display recipes
+    // show empty message
     if (filtered.length > 0) {
         displayRecipes(filtered);
     } else {
         container.innerHTML = `<p class="no-results">No results</p>`;
     }
 };
-
 
 //===============================
 // pick a random recipe
@@ -484,3 +478,115 @@ const filterSearchResults = () => {
 };
 
 searchInput.addEventListener("input", filterSearchResults);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //===============================
+// // filter recipes
+// //===============================
+
+// // save the selected filter here
+// let activeFilters = []
+
+// // show all recipes when "all" button is clicked
+// allBtn.addEventListener("click", () => {
+//     activeFilters = []; //reset filters
+//     displayRecipes(recipes);
+// });
+
+// // 
+// filterBtn.forEach(button => {
+//     if (button.id === "all")return; // ignore the rest of the functions is "all" is clicked
+
+//     button.addEventListener("click", () => {
+//         const cuisine = button.id;
+
+//         if (activeFilters.includes(cuisine)) {
+//             // remove recipes if they are already displayed 
+//             activeFilters = activeFilters.filter(country => country !== cuisine);
+//         } else {
+//             // add the filtered recipes
+//             activeFilters.push(cuisine);
+//         }
+
+//         applyFilters();
+//     });
+// });
+
+// const applyFilters = () => {
+//     // if there are no active filters, then show all recipes
+//     if (activeFilters.length === 0) {
+//         displayRecipes(recipes);
+//         return;
+//     }
+
+//     const filtered = recipe.filter(recipe =>
+//         activeFilters.includes(recipe.cuisines.toLowerCase())
+//     );
+
+//     // if there are active recipes, show the recipes. otherwise show the text
+//     if (filtered.length > 0) {
+//         displayRecipes(filtered);
+//     } else {
+//         container.innerHTML = `<p class="no-results">No results</p>`;
+//     }
+// }
+
+
+// //===============================
+// // sort recipes (sort by time)
+// //===============================
+
+// descBtn.addEventListener("click", () => {
+//     currentSort = "desc";
+//     applyKitchenFilters();
+// })
+
+// ascBtn.addEventListener("click", () => {
+//     currentSort = "asc";
+//     applyKitchenFilters();
+// })
+
+// // filter + sort function
+// const applyKitchenFilters = () => {
+//     let filtered = []; // make an empty box
+
+//     if (activeFilters.length === 0) {
+//         // if no active filter, make a copy of recipes list
+//         filtered = [...recipes];
+//     } else {
+//         // if filters are active, pick up the recipes
+//         filtered = recipes.filter(recipe =>
+//             activeFilters.includes(recipe.cuisine.toLowerCase())
+//         );
+//     }
+
+//     // sort filtered recipes
+//     if (currentSort === "desc") {
+//         filtered.sort((a,b) => b.readyInMinutes - a.readyInMinutes);
+//     } else {
+//         filtered.sort((a,b) => a.readyInMinutes - b.readyInMinutes);
+//     }
+
+//     // display recipes
+//     if (filtered.length > 0) {
+//         displayRecipes(filtered);
+//     } else {
+//         container.innerHTML = `<p class="no-results">No results</p>`;
+//     }
+// };
+
